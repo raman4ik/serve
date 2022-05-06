aa6277064a97d74fde5bd71235b1b6ee514e683d
raman4ik <74449936+raman4ik@users.noreply.github.com>
2022-05-06 22:02:53 +0300

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+// const bodyParser = require('body-parser')
+const errorMiddleware = require('./middlewares/errorMiddleware')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000/'
+}))
+
+//Import Routes
+const product = require('./routes/productRoute')
+
+
+app.use('/api/', product)
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/200.html"))
+})
+
+app.use(errorMiddleware)
+
+module.exports = app
